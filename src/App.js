6c534cd41742969afea902bd8f78b530df65854b
HighGreat <a.b.s2121.ayt@outlook.jp>
2020-01-19 16:38:51 +0900

    functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm React App</h1>
       <p>This is realy working</p>
+      <Person />
     </div>
   );
 }
