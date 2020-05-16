f0f9a0c4eee2d82ca31d813af6c1d0984207794e
Ayato <a.b.s2121.ayt@outlook.jp>
2020-05-16 21:56:15 +0900

    Fix indent

--- src/App.js
+++ src/App.js
@@ -52,19 +52,19 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-          <div>
-            <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age} />
-            <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, "Max!!")}
-                changed={this.nameChangeHandler} >My Hobbies: Racing</Person>
-            <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age} />
-          </div>
+        <div>
+          <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age} />
+          <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, "Max!!")}
+              changed={this.nameChangeHandler} >My Hobbies: Racing</Person>
+          <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age} />
+        </div>
       );
     }
 
